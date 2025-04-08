a1ea66474f43b807fd1d94e1b6f5967dd97573cd
Atibway <ainebyoonaatiidu@gmail.com>
2025-04-08 15:36:50 +0300

    ok

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import LandingPage from './_component/HomePage'
 import { getHostels } from '@/actions/hostels'
 
-interface SearchPageProps {
-  searchParams: {
-    title: string;
-  }
-}
+type SearchParams = Promise<{ [key: string]: string  }>
 
-const HomePage = async ({ searchParams }: SearchPageProps) => {
-  // No need to await searchParams since it's already an object.
+const HomePage = async ({ searchParams }: {searchParams: SearchParams}) => {
+  const { title } = await searchParams
+
   const hostels = await getHostels({
-    title: searchParams.title
+    title: title
   });
   
   return (
