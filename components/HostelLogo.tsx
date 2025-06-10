14228d6952d216b4426fcf2f1d63e467fd246888
Ainebyoona Atidu <142122906+Atibway@users.noreply.github.com>
2025-06-10 16:52:37 +0300

    Update HostelLogo.tsx

--- components/HostelLogo.tsx
+++ components/HostelLogo.tsx
@@ -4,8 +4,7 @@ const HostelLogo: React.FC = () => {
   return (
     <div className="flex items-center justify-center mb-4">
       <div className="text-white font-bold text-2xl">
-        <span className="text-hostel-accent1">Real Time web-Enabled</span>
-        <span className="text-hostel-accent2">Hostel booking system</span>
+        <span className="text-hostel-accent1">Real Time web-Enabled Hostel booking system</span>
       </div>
     </div>
   );
